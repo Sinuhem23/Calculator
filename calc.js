2b8f8c2c2db771fed5a870b62fe9b145a2d9d0f6
sinuhe Montero <sinuhem23@gmail.com>
2021-08-06 10:34:52 -0400

    made some changes on the order of operation

--- calc.js
+++ calc.js
@@ -1,11 +1,22 @@
+// Calculator using Functions! ////////////////////////////////////////////////////////
 var prompt = require('prompt-sync')();
 var user_num1 = Number(prompt('Enter first number: '));
-var user_num2 = Number(prompt('Enter second number: '));
+
 var case_selection = Number(
   prompt(
     'Select between 1-6. 1:Add, 2:Subtract, 3:Multiply, 4:Divide, 5:Average, 6:Exit : '
   )
 );
+
+if (case_selection != 6) {
+  if (case_selection < 6) {
+    var user_num2 = Number(prompt('Enter second number: '));
+  }
+}
+
+if (case_selection == 6) {
+  return console.log('You have now exited');
+}
 
 function calc(num1, num2) {
   switch (case_selection) {
